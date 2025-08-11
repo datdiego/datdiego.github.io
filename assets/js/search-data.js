14e23dc1154fbe6711931f67dae04b7ec7ce43fe
datdiego <diego.alducin@gmail.com>
2025-08-11 03:52:08 +0000

    Deploying to gh-pages from @ datdiego/datdiego.github.io@0e5d976b7b401f918ffd374369f6c604985ef485 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,7 +50,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%69%65%67%6F.%61%6C%64%75%63%69%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
@@ -67,25 +67,11 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/alducin-diego", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=Rli2mWsAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
